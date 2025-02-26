fe84b87b4fb20dc2861c37e900f21f9a5a24f63f
ANA CATARINA <251133647@cfp127aluno.local>
2025-02-26 16:53:49 -0300

    Cálculo de IMC implementado

--- src/br/senai/sp/jandira/clinica/Clinica.java
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -17,7 +17,7 @@ public class Clinica {
 		// Criação dos objetos do tipo paciente
         Paciente p1 = new Paciente();
         p1.nome = "Ana Maria";
-        p1.setAltura(-1.65);
+        p1.setAltura(1.65);
         p1.setPeso(68.200);
         p1.telefone = "(11)988888-8888";
         p1.datadenascimento = LocalDate.of(2003, 2, 28);
@@ -27,9 +27,10 @@ public class Clinica {
         p2.nome = "Ana Catarina";
         p2.datadenascimento = LocalDate.of(2007, 8, 03);
         p2.setAltura(1.68);
-        p2.setPeso(-44.200); 
+        p2.setPeso(44.200); 
         p2.telefone = "(11)93206-9067";
         p2.estaInternado = false;
+        
         
         // Mostrar dados do paciente 1
         p1.mostrarFichaPaciente();
